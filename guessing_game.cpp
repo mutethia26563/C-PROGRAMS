1d2701e99ef25de78cd5ebaa8787c5020dbd2c03
mutethia26563 <mutethiakathuku@gmail.com>
2025-10-09 20:40:47 +0300

    Add files via upload

---
+++ guessing_game.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int secretNumber, guess, attempts = 0;
+
+    // Seed the random number generator
+    srand(time(NULL));
+    secretNumber = rand() % 20 + 1; // Generates a number between 1 and 20
+
+    printf("Welcome to the Number Guessing Game!\n");
+    printf("I'm thinking of a number between 1 and 20.\n");
+
+    // Loop until the correct guess
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess > secretNumber) {
+            printf("Too high!\n");
+        } else if (guess < secretNumber) {
+            printf("Too low!\n");
+        } else {
+            printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+        }
+    } while (guess != secretNumber);
+
+    return 0;
+}
