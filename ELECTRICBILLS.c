91771f5b679f875574293fd2eb5e431c1289bc87
mutethia26563 <mutethiakathuku@gmail.com>
2025-10-23 02:22:46 +0700

    Add files via upload

---
+++ ELECTRICBILLS.c
@@ -0,0 +1,31 @@
+/*
+NAME:MUTETHIA WYCLIFFE KATHUKU
+REG NO:CT101/G/26563/25
+DESCRIPTION:FUNCTION TO CALCULATE ELECTRIC BILLS
+*/
+#include <stdio.h>
+
+float calculateElectricBill(int units) {
+    float bill = 0;
+
+    if (units <= 100) {
+        bill = units * 10;
+    } else if (units <= 200) {
+        bill = (100 * 10) + ((units - 100) * 15);
+    } else {
+        bill = (100 * 10) + (100 * 15) + ((units - 200) * 20);
+    }
+
+    return bill;
+}
+
+int main() {
+    int units;
+    printf("Enter number of units consumed: ");
+    scanf("%d", &units);
+
+    float totalBill = calculateElectricBill(units);
+    printf("Total electric bill: KSh. %.2f\n", totalBill);
+
+    return 0;
+}
